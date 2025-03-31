a0a563da0838303bc658d73e313e329240b7a905
4ndrelim <tiltedaf3@gmail.com>
2025-03-31 17:10:32 +0800

    update training scripts

--- open-instruct/scripts/custom_scripts/merge_lora.sh
+++ open-instruct/scripts/custom_scripts/merge_lora.sh
@@ -1,7 +1,24 @@
-MODEL_SIZE=8b
-MODEL_TOKENIZER_TEMPLATE_PATH=/home/andre/andre/models/meta-llama-Llama-3.1-8B-Instruct
-LORA_LAYERS=/home/andre/andre/models/adaptors/tweet_LLama3.1-8b
-MERGED_MODEL=/home/andre/andre/models/merged/tweet/lora_tuned_llama3.1_8b
+# MODEL_SIZE=8b
+# MODEL_TOKENIZER_TEMPLATE_PATH=/home/andre/andre/models/meta-llama-Llama-3.1-8B-Instruct
+# LORA_LAYERS=/home/andre/andre/models/adaptors/tweet_LLama3.1-8b
+# MERGED_MODEL=/home/andre/andre/models/merged/tweet/lora_tuned_llama3.1_8b
+
+# export CUDA_VISIBLE_DEVICES=2
+
+# accelerate launch \
+#     --mixed_precision bf16 \
+#     --num_machines 1 \
+#     --num_processes 1 \
+#     open_instruct/merge_lora.py \
+#     --base_model_name_or_path $MODEL_TOKENIZER_TEMPLATE_PATH \
+#     --lora_model_name_or_path $LORA_LAYERS \
+#     --output_dir $MERGED_MODEL \
+#     --save_tokenizer
+
+MODEL_SIZE=7b
+MODEL_TOKENIZER_TEMPLATE_PATH=/shared/ssd/models/Qwen2.5-7B-Instruct
+LORA_LAYERS=/home/andre/andre/models/adaptors/sentiment_generation_Qwen2.5-7B-Instruct
+MERGED_MODEL=/home/andre/andre/models/merged/tweet/lora_tuned_sentiment_generation_Qwen2.5-7B-Instruct
 
 export CUDA_VISIBLE_DEVICES=6
 
