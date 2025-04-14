054c340aa564634b4c74b03d9f5f3bf561a037c9
Marcus Pang <marcuspangyuyang@gmail.com>
2025-04-14 23:10:53 +0800

    chore: tidy up script + output

--- evaluation/alpaca.sh
+++ evaluation/alpaca.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# python3 evaluation/prepare_alpaca_data.py --input_csv predictions/mimicry_generation/trump_v4_llama.csv --output_dir evaluation/alpaca_eval_prepared_data/trump_v4_llama --generator_name trump_v4_llama
-
+MODEL_NAME="trump_v4_llama"
 CURRENT_DIR=$(pwd)
 
-alpaca_eval --model_outputs evaluation/alpaca_eval_prepared_data/trump_v4_llama/model_outputs.json \
-            --reference_outputs evaluation/alpaca_eval_prepared_data/trump_v4_llama/reference_outputs.json \
+# python3 evaluation/prepare_alpaca_data.py --input_csv predictions/mimicry_generation/${MODEL_NAME}.csv --output_dir evaluation/alpaca_eval_prepared_data/${MODEL_NAME} --generator_name ${MODEL_NAME}
+
+alpaca_eval --model_outputs evaluation/alpaca_eval_prepared_data/${MODEL_NAME}/model_outputs.json \
+            --reference_outputs evaluation/alpaca_eval_prepared_data/${MODEL_NAME}/reference_outputs.json \
             --annotators_config $CURRENT_DIR/evaluation/configs.yaml \
-            --output_path results/alpaca_mimicry_eval/trump_v4_llama
+            --output_path evaluation/EvalResults/AlpacaMimicry/${MODEL_NAME}
