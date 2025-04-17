d972df062518eeb7ccf307ad4f3fa9518568f090
zxisatree <hcichiazixuan@gmail.com>
2025-04-17 16:23:07 +0800

    Update Slurm scripts

--- open-instruct/spredict.sh
+++ open-instruct/spredict.sh
@@ -2,6 +2,7 @@
 #SBATCH --job-name=pred_lora
 #SBATCH --time=90
 #SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=
 #SBATCH --mem-per-gpu=40G 
 #SBATCH --gpus=a100-40:1
 ###SBATCH --nodelist=xgpg7
@@ -12,11 +13,12 @@ srun nvidia-smi
 
 echo "SBATCH_INFO: Running predict_slurm.py..."
 python3 $HOME/copy-chat/open-instruct/eval/predict_slurm.py \
---model $HOME/copy-chat/models/merged/tweet/p2_Llama-3.1-8B_v03 \
---input_files $HOME/copy-chat/open-instruct/datasets/formatted_datasets/tsad_03_test_senrep.jsonl \
---output_file $HOME/copy-chat/predictions/sentiment_generation/tsad_p2_llama_v03_senrep.jsonl \
+--model $HOME/copy-chat/models/merged/tweet/p2_Llama-3.1-8B_v11 \
+--input_files $HOME/copy-chat/open-instruct/datasets/formatted_datasets/tsad_08_test_preprocessed.jsonl \
+--output_file $HOME/copy-chat/predictions/sentiment_generation/tsad_llama_v11_preprocessed_augment.jsonl \
 --use_vllm \
 --use_chat_format \
 --max_new_tokens 256
 
+#--model $HOME/copy-chat/models/meta-llama-Llama-3.1-8B-Instruct \
 #--stop_token '<|system|>' \
